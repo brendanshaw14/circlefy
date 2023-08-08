bc9a38d9d433c7cdddf3b589dab98b56127a1eec
brendanshaw14 <brendanshaw18@gmail.com>
2023-08-08 00:11:26 -0400

    Home page mostly finished

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -60,17 +60,16 @@ const Login = () => {
         </header>
         <main className="body-container"> 
             <div className="circle-container">
-                <Circle x = '35' y = '40' size = '40' color="#ff9f3d" text="Your Spotify Wrapped, All Year Round " />
+                <Circle x = '35' y = '45' size = '40' color="#ff9f3d" text="Your Spotify Wrapped, All Year Round " />
                 <Circle x = '90' y = '30' sisze = '10' color="#ff59b5"/> 
-                <Circle x = '90' y = '80' size = "10" image="images/KaliUchis-Isolation.png" />
-                <Circle x = '60' y = '15' size = "10" image="images/DuaLipa-FutureNostalgia.png" />
-                <Circle x = '10' y = '10' size = "8" image="images/Khalid-AmericanTeen.jpg" scale = "1.045"/>
+                <Circle x = '95' y = '80' size = "20" image="images/KaliUchis-Isolation.png" />
+                <Circle x = '60' y = '15' size = "15" image="images/DuaLipa-FutureNostalgia.png" />
+                <Circle x = '8' y = '10' size = "20" image="images/Khalid-AmericanTeen.jpg" scale = "1.045"/>
                 <Circle x = '55' y = '80' size = "12" image="images/TravisScott-Astroworld.png"/>
-                <Circle x = '70' y = '70' size = '20' color="#ff59b5" text="piss"/> 
-                <Circle x = '12' y = '75' size = '15' color="#399fec" text="Circle 3" />
-                <Circle x = '80' y = '30' size = '20' color="#a8df85" text="Circle 4" />
-                {/*<Circle color="#a8df85" size = "10" text="Circle 3" />
-    <Circle color="#399fec" size = "10" text="Circle 3" />*/}
+                <Circle x = '79' y = '55' size = '20' color="#ff59b5" text="Login with your account below"/> 
+                <Circle x = '12' y = '75' size = '15' color="#399fec" text="" />
+                <Circle x = '85' y = '10' size = '15' color="#a8df85" text="See top tracks, artists, and more" />
+                <Circle x = '70' y = '95' size = '10' color="rgb(233, 215, 61)"/>
             </div>
             <div className="button-container">
                 <button className="login-button" onClick={handleLogin}>Login with Spotify</button>
